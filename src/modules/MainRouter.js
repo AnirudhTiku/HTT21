2a4b2338dd057c7d3a1a44fe80a83f401d7817ae
Taylor Xi <caetortaylor@gmail.com>
2021-01-16 17:10:09 -0500

    Added FloatMode to router

--- src/modules/MainRouter.js
+++ src/modules/MainRouter.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import GravityMode from './GravityMode';
+import FloatMode from './FloatMode';
 import Main from './Main';
 
 function MainRouter(props){
@@ -8,7 +9,7 @@ function MainRouter(props){
     return(
         <Switch>
             <Route exact path="/float">
-
+                <FloatMode />
             </Route> 
             <Route exact path="/gravity">
                 <GravityMode/>
