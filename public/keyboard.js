3ccf61b5cdc516bd2ebb3928601d1f6c72abe360
AnirudhTiku <62532928+AnirudhTiku@users.noreply.github.com>
2021-01-16 18:38:43 -0500

    Update keyboard.js

--- public/keyboard.js
+++ public/keyboard.js
@@ -1,3 +1,10 @@
 window.addEventListener("keydown", (event) => {
     console.log(event);
-});
+
+    var today = new Date();
+
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + ":" + today.getMilliseconds();
+    console.log(time);
+
+}
+)
