b6015e94180c4c6c541f3417c13bb8aa926da16c
Yang Lu <xinyanglu664@gmail.com>
2021-01-16 19:46:33 -0500

    add name to main page

--- src/modules/Main.js
+++ src/modules/Main.js
@@ -7,50 +7,47 @@ export default function Main(props) {
 
 
     return (
-
-        <Grid 
-            container 
+        <div 
             style={{ 
-                display: 'flex', 
-                justifyContent: 'center', 
-                alignItems: 'center', height: '100vh', 
-                backgroundImage: `url('/keyboard_cartoon.png')`,
-                backgroundPosition: 'center',
-                backgroundSize:'cover' }}
+            display: 'flex', 
+            justifyContent: 'center', 
+            alignItems: 'center',   
+            height: '100vh', 
+            backgroundImage: `url('/keyboard_cartoon.png')`,
+            backgroundPosition: 'center',
+            backgroundSize:'cover' }}
         >
+            <Grid 
+                container 
+                spacing={24}
+                style={{display:'flex', justifyContent:'center'}}
+            >
+                <Grid item xs={12} style={{display: 'flex', justifyContent:'center'}}>
+                    <h1>KEYBored</h1>
+                </Grid>
 
-            <Grid item xs={2} style={{display:'flex',justifyContent:'center', height:'6%'}}>
-                <Button 
-                    variant="contained" color='primary' 
-                    onClick={()=>history.push('/float')}
-                    style={{borderRadius: '10%', width:'50%'}}
-                >
+                <Grid item xs={2} style={{display:'flex',justifyContent:'center'}}>
+                    <Button 
+                        variant="contained" color='primary' 
+                        onClick={()=>history.push('/float')}
+                        style={{borderRadius: '10%', width:'50%'}}
+                    >
 
-                    Float
-                </Button>
+                        Float
+                    </Button>
+                </Grid>
+
+                <Grid item xs={2} style={{display:'flex', justifyContent:'center'}}>
+                    <Button 
+                        variant="contained" color='primary' size="Large" 
+                        onClick={()=>history.push('/gravity')}
+                        style={{borderRadius: '10%', width:'50%'}}
+                    >
+
+                        Gravity
+                    </Button>
+                </Grid>
             </Grid>
-
-            <Grid item xs={2} style={{display:'flex', justifyContent:'center', height:'6%'}}>
-                <Button 
-                    variant="contained" color='primary' size="Large" 
-                    onClick={()=>history.push('/gravity')}
-                    style={{borderRadius: '10%', width:'50%'}}
-                >
-
-                    Gravity
-                </Button>
-            </Grid>
-
-            <Grid item xs={2} style={{ display:'flex', justifyContent:'center', height:'6%'}}>
-                <Button 
-                    variant="contained" color='primary' size="Large" 
-                    onClick={()=>history.push('/destroy')}
-                    style={{borderRadius:'10%', width:'50%'}}
-                >
-
-                    Destroy
-                </Button>
-            </Grid>
-        </Grid>
+        </div>
     );
 }
