df3c88ee8fb55c133168cf241cc2695de1aabc9d
Yang Lu <xinyanglu664@gmail.com>
2021-01-16 21:04:15 -0500

    Make visual improvements to float

--- src/Modules/FloatMode.js
+++ src/Modules/FloatMode.js
@@ -1,6 +1,8 @@
-import React, { useEffect } from 'react'
-import ReactDOM from 'react-dom'
-import Matter from 'matter-js'
+import React from 'react';
+import Matter from 'matter-js';
+import { Button } from '@material-ui/core';
+import DeleteSweepIcon from '@material-ui/icons/DeleteSweep';
+import './FloatMode.css'
 
 class FloatMode extends React.Component{
     constructor(props) {
@@ -8,79 +10,88 @@ class FloatMode extends React.Component{
         this.state = {balls: []};
       }
     
-      componentDidMount() {
+    componentDidMount() {
+      
+      const worldHeight = window.innerHeight-42;
+      const worldWidth = window.innerWidth-50;
         var Engine = Matter.Engine,
-          Render = Matter.Render,
-          World = Matter.World,
-          Bodies = Matter.Bodies,
-          Body = Matter.Body,
-          Mouse = Matter.Mouse,
-          MouseConstraint = Matter.MouseConstraint,
-          Vector = Matter.Vector;
-    
-        var engine = Engine.create();
-        
-        engine.world.gravity.y = 0;
+        Render = Matter.Render,
+        World = Matter.World,
+        Bodies = Matter.Bodies,
+        Body = Matter.Body,
+        Mouse = Matter.Mouse,
+        MouseConstraint = Matter.MouseConstraint,
+        Vector = Matter.Vector;
+  
+      var engine = Engine.create();
+      
+      engine.world.gravity.y = 0;
 
-        var render = Render.create({
-          element: this.refs.scene,
-          engine: engine,
-          options: {
-            width: 1920,
-            height: 965,
-            wireframes: false,
+      var render = Render.create({
+        element: this.refs.scene,
+        engine: engine,
+        options: {
+          width: worldWidth,
+          height: worldHeight,
+          wireframes: false,
+          background:'rgb(0,0,0)'
+        }
+      });
+  
+      World.add(engine.world, [
+        // walls
+        Bodies.rectangle(worldWidth/2, 0, worldWidth, 10, { isStatic: true, restitution: 1, friction: 0 }),
+        Bodies.rectangle(worldWidth/2, worldHeight, worldWidth, 10, { isStatic: true, restitution: 1, friction: 0 }),
+        Bodies.rectangle(0, worldHeight/2, 10, worldHeight, { isStatic: true, restitution: 1, friction: 0 }),
+        Bodies.rectangle(worldWidth, worldHeight/2, 10, worldHeight, { isStatic: true, restitution: 1, friction: 0 })
+      ]);
+  
+      World.add(engine.world, [ballA, ballB]);
+  
+      // add mouse control
+      var mouse = Mouse.create(render.canvas),
+        mouseConstraint = MouseConstraint.create(engine, {
+          mouse: mouse,
+          constraint: {
+            stiffness: 0.2,
+            render: {
+              visible: false
+            }
           }
         });
-    
-        var ballA = Bodies.circle(210, 100, 30, { restitution: 1, frictionAir: 0, friction: 0 });
-        var ballB = Bodies.circle(110, 50, 30, { restitution: 1, frictionAir: 0, friction: 0 });
-        World.add(engine.world, [
-          // walls
-          Bodies.rectangle(960, 0, 1920, 100, { isStatic: true, restitution: 1, friction: 0 }),
-          Bodies.rectangle(960, 965, 1920, 100, { isStatic: true, restitution: 1, friction: 0 }),
-          Bodies.rectangle(0, 483, 100, 966, { isStatic: true, restitution: 1, friction: 0 }),
-          Bodies.rectangle(1920, 483, 100, 966, { isStatic: true, restitution: 1, friction: 0 })
-        ]);
-    
-        World.add(engine.world, [ballA, ballB]);
-    
-        // add mouse control
-        var mouse = Mouse.create(render.canvas),
-          mouseConstraint = MouseConstraint.create(engine, {
-            mouse: mouse,
-            constraint: {
-              stiffness: 0.2,
-              render: {
-                visible: false
-              }
-            }
-          });
-    
-        World.add(engine.world, mouseConstraint);
-    
-        // Matter.Events.on(mouseConstraint, "mousedown", function(event) {
-        //   World.add(engine.world, Bodies.circle(150, 50, 30, { restitution: 0.7 }));
-        // });
+  
+      World.add(engine.world, mouseConstraint);
+  
+      // Matter.Events.on(mouseConstraint, "mousedown", function(event) {
+      //   World.add(engine.world, Bodies.circle(150, 50, 30, { restitution: 0.7 }));
+      // });
 
-        document.addEventListener('keydown', event => {
-            var ball = Bodies.circle(Math.random()*1800, Math.random()*860, 30, { restitution: 1, frictionAir: 0, friction: 0  })
-            var velocity = Vector.create(Math.random()*50 - 25, Math.random()*50 - 25)
-            Body.setVelocity(ball, velocity)
-            this.state.balls.push(ball);
-            World.add(engine.world, ball);
-            if(this.state.balls.length > 50){
-              World.remove(engine.world, this.state.balls[this.state.balls.length-50])
-              this.state.balls.splice(1,0)
-            }
-        });
-    
-        Engine.run(engine);
-    
-        Render.run(render);
-      }
+      document.addEventListener('keydown', event => {
+          var ball = Bodies.circle(Math.random()*1800, Math.random()*860, 30, { restitution: 1, frictionAir: 0, friction: 0  })
+          var velocity = Vector.create(Math.random()*50 - 25, Math.random()*50 - 25)
+          Body.setVelocity(ball, velocity)
+          this.state.balls.push(ball);
+          World.add(engine.world, ball);
+          if(this.state.balls.length > 50){
+            World.remove(engine.world, this.state.balls[this.state.balls.length-50])
+            this.state.balls.splice(1,0)
+          }
+      });
+  
+      Engine.run(engine);
+  
+      Render.run(render);
+    }
     
       render() {
-        return <div ref="scene" />;
+        return (
+        <div ref="scene" style={{backgroundColor:'black', width:'100wh', height:'100vh'}}>
+          <Button variant="contained" color="secondary" onClick={()=>console.log('data')}>
+            Show Data
+          </Button>
+          <DeleteSweepIcon style={{fill:'orange', cursor:'pointer'}}/>
+        </div>
+        );
       }
     }
 
